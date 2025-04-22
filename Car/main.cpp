209f23f048f05b2a648755193f86d722ea7000db
hawri <hawri@DESKTOP-BMM1386>
2025-04-22 18:48:10 +0300

    Add project files.

---
+++ Car/main.cpp
@@ -0,0 +1,71 @@
+﻿#include<iostream>
+using namespace std;
+
+#define MIN_TANK_CAPACITY	 20
+#define MAX_TANK_CAPACITY	120
+
+class Tank
+{
+	double fuel_level;
+public:
+	const int CAPACITY;
+	double get_fuel_level()const
+	{
+		return fuel_level;
+	}
+	Tank(int capacity) :CAPACITY
+		(
+			capacity < MIN_TANK_CAPACITY ? MIN_TANK_CAPACITY:
+			capacity > MAX_TANK_CAPACITY ? MAX_TANK_CAPACITY:
+			capacity
+		),
+		fuel_level(0)
+	{
+		cout << "Tank:";
+		if (capacity < MIN_TANK_CAPACITY)cout << "Min capacity was applied" << endl;
+		if (capacity > MAX_TANK_CAPACITY)cout << "Max capacity was applied" << endl;
+		//this->CAPACITY = capacity;
+		//this->fuel_level = 0;
+		cout << "Tank is ready" << endl;
+	}
+	~Tank()
+	{
+		cout << "Tank is over" << endl;
+	}
+	double fill(double amount)
+	{
+		if (amount < 0)return fuel_level;
+		fuel_level += amount;
+		if (fuel_level >= CAPACITY)
+		{
+			fuel_level = CAPACITY;
+			cout << "Full Tank" << endl;
+		}
+		return fuel_level;
+	}
+	double give_fuel(double amount)
+	{
+		fuel_level -= amount;
+		if (fuel_level < 0)fuel_level = 0;
+		return fuel_level;
+	}
+	void info()const
+	{
+		cout << "Capacity:\t" << CAPACITY << " liters.\n";
+		cout << "Fuel level:\t" << fuel_level << " liters.\n";
+	}
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	Tank tank(80);
+	double fuel;
+	do
+	{
+		cout << "На сколько заправляемся? "; cin >> fuel;
+		tank.fill(fuel);
+		tank.info();
+	} while (true);
+	tank.info();
+}
